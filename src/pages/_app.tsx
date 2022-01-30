305357ec49fe18ea9c1d6a68a44578e9bac1139e
andrey-maslov <andrei.maslau@gmail.com>
2022-01-30 21:02:35 +0100

    scrollToTop hidden

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import App from 'next/app';
 import '../assets/scss/index.scss';
 import 'focus-visible/dist/focus-visible.js';
 import { ToastProvider } from 'react-toast-notifications';
-// @ts-ignore
-import ScrollToTop from '../components/common/ScrollToTop';
+// import ScrollToTop from '../components/common/ScrollToTop';
 import { getCookie } from '../helper/cookie';
 import { appWithTranslation } from 'next-i18next';
 
@@ -15,7 +14,7 @@ interface AppProps {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ToastProvider autoDismiss autoDismissTimeout={5000} placement="bottom-left">
-      <ScrollToTop />
+      {/*<ScrollToTop />*/}
       <Component {...pageProps} />
     </ToastProvider>
   );
