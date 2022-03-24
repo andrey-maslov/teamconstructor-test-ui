a2cb817a7d0af6e3c228d9883e00adda484b87f3
andrey-maslov <andrei.maslau@gmail.com>
2022-03-24 19:14:29 +0100

    admin: page size increased from 25 to 40 items

--- src/api/psychologicalTestsAPI.ts
+++ src/api/psychologicalTestsAPI.ts
@@ -1,27 +1,20 @@
 import axios from 'axios';
-import { getCookieFromBrowser } from '../helper/cookie';
 import { decodeBase64 } from '../helper/helper';
 import { IGetTestsResponse } from '../typings/types';
 import { DecodedDataType } from '../libs/psychology/build/main/types/types';
-import {API_TESTS} from "../constants/constants";
+import { API_TESTS } from '../constants/constants';
 
-export const getAuthConfig = (jwt: string) => {
-  return {
-    headers: {
-      Authorization: `Bearer ${jwt}`,
-    },
-  };
-};
-
-export function getDecodedTestData(testList: IGetTestsResponse[]): DecodedDataType {
-  const neededTest: IGetTestsResponse = testList.filter((test) => test.type === 0)[0];
+export function getDecodedTestData(
+  testList: IGetTestsResponse[]
+): DecodedDataType {
+  const neededTest: IGetTestsResponse = testList.filter(
+    (test) => test.type === 0
+  )[0];
   const decodedData = decodeBase64(neededTest.value);
   return JSON.parse(decodedData);
 }
 
 export async function saveTestResult(dataToSave): Promise<any> {
-  const token = getCookieFromBrowser('token');
-
   try {
     const response = await axios.post(API_TESTS, { data: dataToSave });
     if (response?.data?.data?.attributes) {
@@ -32,16 +25,15 @@ export async function saveTestResult(dataToSave): Promise<any> {
   }
 }
 export function saveTestResultToFile(dataToSave): any {
-  axios.post('save-data', dataToSave)
-      .then(r => console.log('success'))
-      .catch(err => console.error(err))
+  axios
+    .post('save-data', dataToSave)
+    .then((r) => console.log('success'))
+    .catch((err) => console.error(err));
 }
 
 export async function getAllTestResults(): Promise<any> {
-  const token = getCookieFromBrowser('token');
-
   try {
-    const response = await axios(API_TESTS);
+    const response = await axios(`${API_TESTS}?pagination[pageSize]=50&sort=createdAt:DESC`);
     if (response?.data) {
       return response.data;
     }
